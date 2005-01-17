68209cd9cc0e331048e4ee03cde96db2e5f4b464
Kris Jurka <books@ejurka.com>
2005-01-17 11:46:59 +0000

    Update website for official 8.0 release.  Update license and copyright years to 2005.  Remove integrated documentation produced by forrest in favor of including documentation produced from the source tree by the older xslt process.  This produces better output and allows it to split into chapters.

--- scripts/buildSite.sh
+++ scripts/buildSite.sh
@@ -7,9 +7,11 @@ cd pgjdbc && cvs update && cd ..
 cd pgjdbc && ./update-translations.sh && cd ..
 mkdir src/documentation/content/xdocs/development/po
 for i in pgjdbc/org/postgresql/translation/*.po; do echo $i; msgfmt -c -v -o /dev/null $i; done 2>&1 | perl scripts/generatePoStatus.pl > src/documentation/content/xdocs/development/po/status.xml
-cd pgjdbc && ant publicapi privateapi && cd ..
+cd pgjdbc && ant publicapi privateapi doc && cd ..
 forrest
 cp -r pgjdbc/build/publicapi build/site/documentation
 cp -r pgjdbc/build/privateapi build/site/development
 cp pgjdbc/org/postgresql/translation/messages.pot pgjdbc/org/postgresql/translation/*.po build/site/development/po
+mkdir build/site/documentation/80
+cp pgjdbc/build/doc/*.html build/site/documentation/80
 
